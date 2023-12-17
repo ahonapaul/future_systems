843624c6b6ffe939daa27065e556b93a6c02e2ac
Ahona Paul <paulahona03@gmail.com>
2023-12-17 15:55:19 -0500

    finalsss

--- threejs_script.js
+++ threejs_script.js
@@ -1,52 +1,50 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { ArcballControls } from 'three/addons/controls/ArcballControls.js';
+import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+//npx vite on terminal to run this page (see three.js docs)
 
-//npm vite on terminal to run this page (see three.js docs)
-
-const loader = new GLTFLoader();
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 
-const light = new THREE.AmbientLight(0xfff5d4); // soft white light
+const light = new THREE.AmbientLight(0xfff5d4); 
 scene.add(light); 
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild(renderer.domElement);
 
-camera.position.z = -10;
-camera.position.x = 10;
-camera.position.y = 18;
+camera.position.z = -30;
+camera.position.x = 30;
+camera.position.y = 20;
 
 camera.rotateX(0);
 camera.rotateY(-90);
 camera.rotateZ(-90);
 
 scene.position.y = 10;
-scene.position.x = 10;
-scene.position.z = -10;
+scene.position.x = 20;
+scene.position.z = -20;
 
-loader.load("fas_room1.glb", function (gltf) {
+const loader = new GLTFLoader();
+
+loader.load("animation.gltf", function (gltf) {
 	scene.add(gltf.scene);
 });
+
+
+const controls = new OrbitControls( camera, renderer.domElement );
 
 function animate() { 
 	requestAnimationFrame(animate);
 	renderer.render( scene, camera ); 
+	controls.update();
+
 } 
 
 animate();
 
-const controls = new ArcballControls( camera, renderer.domElement, scene );
 
-controls.addEventListener( 'change', function () {
-
-	renderer.render( scene, camera );
-
-} );
-
-controls.update();
